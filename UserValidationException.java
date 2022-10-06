192d59695f1c4b3087e46798bcae5f37d4afc739
Azhar <azharumi786@gmail.com>
2022-10-06 18:40:14 +0530

    User registration using lambda expression

---
+++ UserValidationException.java
@@ -0,0 +1,18 @@
+package com.bl.lambda;
+
+public class UserValidationException extends Exception {
+	public ExceptionType type;
+	
+	public enum ExceptionType{
+		ENTERED_NULL, ENTERED_EMPTY
+	}
+
+	public UserValidationException() {
+
+	}
+
+	public UserValidationException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
